b49e40972e7ef673536526929681cf16afe47cb6
romeo Asinari Di San Marzano <ralonsoad@gmail.com>
2023-08-14 16:01:26 +0200

    Generate Questions controller_and_form_2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,21 @@
 class QuestionsController < ApplicationController
 
-    def ask; end
+    def ask
 
-    def answer; end
+    end
+
+    def answer
+         @answer = params[:query]
+
+         @coach = if @answer.end_with?("?")
+               "Silly question, get dressed and go to work!."  
+        
+        elsif @answer = "I am going to work"
+         "Great!"   
+    
+        else
+        "I don't care, get dressed and go to work!" 
+        end
+    end   
+    
 end
